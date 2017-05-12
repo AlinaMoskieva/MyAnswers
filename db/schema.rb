92344efb22b556d6dcd04b1b407a6df49dfe84f3
Alina Moskieva <alinamoskieva@gmail.com>
2017-05-12 18:55:37 +0300

    Add unit model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170508095759) do
+ActiveRecord::Schema.define(version: 20170512154839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,14 @@ ActiveRecord::Schema.define(version: 20170508095759) do
     t.datetime "updated_at", null: false
     t.index ["course_id"], name: "index_subscriptions_on_course_id", using: :btree
     t.index ["user_id"], name: "index_subscriptions_on_user_id", using: :btree
+  end
+
+  create_table "units", force: :cascade do |t|
+    t.integer  "course_id",  null: false
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_units_on_course_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
