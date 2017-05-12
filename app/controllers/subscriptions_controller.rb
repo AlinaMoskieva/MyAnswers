08839128473a5000f15cc366e8753a99d7693d08
Alina Moskieva <alinamoskieva@gmail.com>
2017-05-12 19:39:06 +0300

    add index and show for courses

---
+++ app/controllers/subscriptions_controller.rb
@@ -0,0 +1,7 @@
+class SubscriptionsController < ApplicationController
+  def new
+  end
+
+  def destroy
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170508095759) do
+ActiveRecord::Schema.define(version: 20170512161529) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170508095759) do
     t.string   "description", null: false
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "title",       null: false
   end
 
   create_table "subscriptions", force: :cascade do |t|
@@ -28,6 +29,14 @@ ActiveRecord::Schema.define(version: 20170508095759) do
     t.datetime "updated_at", null: false
     t.index ["course_id"], name: "index_subscriptions_on_course_id", using: :btree
     t.index ["user_id"], name: "index_subscriptions_on_user_id", using: :btree
+  end
+
+  create_table "units", force: :cascade do |t|
+    t.integer  "course_id",  null: false
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_units_on_course_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
