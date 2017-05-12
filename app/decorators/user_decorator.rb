4e7820e58e06ad3e977aa7e26e1364d50a466290
Alina Moskieva <alinamoskieva@gmail.com>
2017-05-12 19:54:19 +0300

    As user i am able to subscribe on course

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -4,4 +4,8 @@ class UserDecorator < ApplicationDecorator
   def full_name_with_email
     "#{object.full_name} (#{object.email})"
   end
+
+  def subscribed?(course_id)
+    Subscription.where(course_id: course_id, user_id: id).empty?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "users/registrations" }
   root to: "pages#home"
+
   resources :courses, only: %i(index show)
+  resources :subscriptions, only: %i(create destroy)
 end
