4df469e75d2d740a596cdb1ba2a2acc3167c52f5
Alina Moskieva <alinamoskieva@gmail.com>
2017-05-13 23:40:04 +0300

    remove confirmable module

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable, :confirmable,
+
+  devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :trackable, :validatable
 
   validates :full_name, presence: true
