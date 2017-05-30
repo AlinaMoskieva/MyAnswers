d7a3ce1dea91d9bd0490315ff27906fd1d3c4adf
Alina Moskieva <alinamoskieva@gmail.com>
2017-05-30 17:48:18 +0300

    remove course_id from unit

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,9 +1,8 @@
 class Unit < ApplicationRecord
-  belongs_to :course
   belongs_to :subject
   has_many :exercises, dependent: :destroy
   has_many :user_units, dependent: :destroy
   has_many :users, through: :user_units
 
-  validates :name, :course, :description, presence: true
+  validates :name, :subject, :description, presence: true
 end
