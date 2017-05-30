b363f21b91f887c1e53e7a2fb9413b86ef8d6306
Alina Moskieva <alinamoskieva@gmail.com>
2017-05-30 19:07:53 +0300

    learn process

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -5,4 +5,12 @@ class Unit < ApplicationRecord
   has_many :users, through: :user_units
 
   validates :name, :subject, :description, presence: true
+
+  def last_exercises?(index)
+    exercises.count == index
+  end
+
+  def next_exercise(index)
+    exercises.find_by(index: index + 1)
+  end
 end
