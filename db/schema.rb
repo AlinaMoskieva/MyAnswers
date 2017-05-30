18be19c55df21ac401ff7c609d1fbda5e92633df
Alina Moskieva <alinamoskieva@gmail.com>
2017-05-30 18:22:08 +0300

    wip

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170528184541) do
+ActiveRecord::Schema.define(version: 20170530145601) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20170528184541) do
     t.text     "theory"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "index",      null: false
     t.index ["unit_id"], name: "index_exercises_on_unit_id", using: :btree
   end
 
@@ -61,13 +62,11 @@ ActiveRecord::Schema.define(version: 20170528184541) do
   end
 
   create_table "units", force: :cascade do |t|
-    t.integer  "course_id",   null: false
     t.string   "name",        null: false
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "description", null: false
     t.integer  "subject_id",  null: false
-    t.index ["course_id"], name: "index_units_on_course_id", using: :btree
   end
 
   create_table "user_units", force: :cascade do |t|
