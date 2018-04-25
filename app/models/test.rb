fb62764deddc39c9f904def33caa3be8c7c6785a
Ramilya Nigmatullina <ramilya.nigmatullina@flatstack.com>
2018-04-25 17:30:28 +0300

    Try to add remove repetitions link

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,7 +2,7 @@ class Test < ApplicationRecord
   has_many :test_questions
   has_many :repetitions
 
-  accepts_nested_attributes_for :repetitions, allow_destroy: :true
+  accepts_nested_attributes_for :repetitions, allow_destroy: true
 
   validates :name, presence: true
 end
