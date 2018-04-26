815ad56f3fbd22af8fab4cc2587083ef5111f219
Alina Moskieva <alina.moskieva@flatstack.com>
2018-04-26 22:06:24 +0300

    Replace methods to model

--- app/decorators/topic_decorator.rb
+++ app/decorators/topic_decorator.rb
@@ -1,12 +1,3 @@
 class TopicDecorator < ApplicationDecorator
-  delegate :id, :name, :children
-  delegate :parent, to: :object
-
-  def has_parent?
-    parent.present?
-  end
-
-  def has_children?
-    children.present?
-  end
+  delegate :id, :name, :children, :parent, :has_parent?, :has_children?
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,4 +3,12 @@ class Topic < ApplicationRecord
 
   belongs_to :parent, class_name: "Topic", optional: true
   has_many :children, class_name: "Topic", foreign_key: "parent_id", dependent: :destroy
+
+  def has_parent?
+    parent.present?
+  end
+
+  def has_children?
+    children.present?
+  end
 end
