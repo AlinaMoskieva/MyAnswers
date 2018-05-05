3f5b5a0912bd72ef22d3e7305a2b89406798ef39
RamiliaNigmatullina <RamiliaNigmatullina@gmail.com>
2018-05-05 23:20:14 +0300

    Add callback

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,4 +5,10 @@ class Test < ApplicationRecord
   accepts_nested_attributes_for :repetitions, allow_destroy: true
 
   validates :name, presence: true
+
+  before_save :define_sort_index
+
+  def define_sort_index
+    self.sort_index = Tests::FindMaxSortIndex.call.sort_index + 1
+  end
 end
