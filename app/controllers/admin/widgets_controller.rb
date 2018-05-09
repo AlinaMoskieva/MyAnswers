62604606834bdb17d48bb1ef4debb818d127b369
Alina Moskieva <alina.moskieva@flatstack.com>
2018-05-09 20:16:11 +0300

    Remove widgets policy

--- app/controllers/admin/widgets_controller.rb
+++ app/controllers/admin/widgets_controller.rb
@@ -2,10 +2,5 @@ module Admin
   class WidgetsController < BaseController
     expose :widgets, -> { Widget.all }
     expose :test_question, -> { TestQuestion.last }
-    # before_action :authorize_resource
-
-    # def authorize_resource
-    #   authorize unit
-    # end
   end
 end
